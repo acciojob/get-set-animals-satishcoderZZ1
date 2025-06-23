1bec7543fe179027fa21f193046be9aff96e7a53
satishcoderZZ1 <167805933+satishcoderZZ1@users.noreply.github.com>
2025-06-23 12:43:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 //complete this code
 class Animal {
-	private _species: string;
 	
-	constructor(species: string){
-		this._species=species;
+	
+	constructor(species){
+		this.species=species;
 	}
 
-	get species(): string{
-		return this._species;
+	get species(){
+		return this.species;
 	}
 	
-	makeSound(): void{
+	makeSound(){
 		console.log(`The ${this.species} make a sound`);
 	}
 }
 
 class Dog extends Animal {
-	bark(): void{
+	bark(){
 		console.log("woof");
 	}
 	
 }
 
 class Cat extends Animal {
-	purr(): void{
+	purr(){
 		console.log("purr");
 	}
 }
